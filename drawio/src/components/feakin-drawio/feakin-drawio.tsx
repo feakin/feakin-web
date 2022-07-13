88edb82afdb7ea421be3f35d3b06202dfacad44f
Phodal Huang <h@phodal.com>
2022-07-13 20:53:40 +0800

    feat: try to decode data

--- drawio/src/components/feakin-drawio/feakin-drawio.tsx
+++ drawio/src/components/feakin-drawio/feakin-drawio.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 import './assets/embed.js';
 
 @Component({
@@ -30,30 +30,58 @@ export class FeakinDrawio {
     graph.setConnectable(false);
     graph.setEnabled(false);
 
-    const node = this.parseXml(this.decode(text));
+    let decoded = this.decode(text);
+    const node = this.parseXml(decoded);
+    console.log(node);
     let cells = graph.importGraphModel(node.documentElement);
-    console.log(cells);
+    this.fitWindow(graph, cells);
+  }
+
+  fitWindow(graph, cells) {
+    var bounds = graph.getGraphBounds();
+
+    var t = graph.view.translate;
+    var s = graph.view.scale;
+
+    bounds.x = bounds.x / s - t.x;
+    bounds.y = bounds.y / s - t.y;
+    bounds.width /= s;
+    bounds.height /= s;
+
+    graph.view.translate.x = - t.x;
+    graph.view.translate.y = - t.y;
+
+    graph.setSelectionCells(cells)
+    graph.scrollCellToVisible(graph.getSelectionCell());
+    graph.moveCells(cells, 0 - bounds.x, 0 - bounds.y)
+
+    graph.fitWindow(bounds, 10, 10)
   }
 
   private decode(text: string) {
-    let node = this.parseXml(text);
+    try {
+      console.log(".............");
 
-    if (node != null && node.nodeName == 'mxfile') {
-      const diagrams = node.getElementsByTagName('diagram');
-      if (diagrams.length > 0) {
-        let data = this.getTextContent(diagrams[0]);
-        data = atob(data);
-        data = (window as any).pako.inflateRaw(Uint8Array.from(data, (c: any) => c.charCodeAt(0)), { to: 'string' });
-        data = decodeURIComponent(data);
-        return data;
+      var node = this.parseXml(text);
+
+      if (node != null && node.firstChild.nodeName == 'mxfile') {
+        const diagrams = node.getElementsByTagName('diagram');
+        if (diagrams.length > 0) {
+          let data = this.getTextContent(diagrams[0]);
+          data = atob(data);
+          data = (window as any).pako.inflateRaw(Uint8Array.from(data, (c: any) => c.charCodeAt(0)), { to: 'string' });
+          data = decodeURIComponent(data);
+          return data;
+        }
       }
+    } catch (e) {
+      throw e;
     }
   }
 
   private parseXml(text: string) {
-    var parser = new DOMParser();
-    let node = parser.parseFromString(text, 'text/xml');
-    return node;
+    let parser = new DOMParser();
+    return parser.parseFromString(text, 'text/xml');
   }
 
   private getTextContent(node) {
