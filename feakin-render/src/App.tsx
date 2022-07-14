b720f63e13c020548d25f984e2ca8cd4995cd1a4
Phodal Huang <h@phodal.com>
2022-07-14 22:09:45 +0800

    feat: init draw

--- feakin-render/src/App.tsx
+++ feakin-render/src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import './App.css';
-import ColoredRect from "./components/ColoredRect";
+import FkRect from "./components/FkRect";
 import { Layer, Stage, Text } from "react-konva";
 
 function App() {
-    return (
-        <Stage width={ window.innerWidth } height={ window.innerHeight }>
-            <Layer>
-                <Text text="Try click on rect"/>
-                <ColoredRect/>
-            </Layer>
-        </Stage>
-    );
+  const [selectedId, selectShape] = React.useState(null as any);
+
+  const checkDeselect = (e: any) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
+
+  return (
+    <Stage width={ window.innerWidth }
+           height={ window.innerHeight }
+           onMouseDown={ checkDeselect }
+           onTouchStart={ checkDeselect }>
+      <Layer>
+        <FkRect
+          isSelected={ selectedId == 1 }
+          onSelect={ () => {
+            selectShape(1);
+          } }/>
+      </Layer>
+    </Stage>
+  );
 }
 
 export default App;
---
+++ feakin-render/src/components/FkRect.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { Rect, Transformer } from 'react-konva';
+
+interface FkRectProps {
+  isSelected: boolean;
+  onSelect: () => void;
+}
+
+function FkRect(props: FkRectProps) {
+  const shapeRef: any = React.useRef();
+  const trRef: any = React.useRef();
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  const [position, setPosition] = useState({
+    x: 20,
+    y: 20,
+    width: 50,
+    height: 50
+  })
+
+  useEffect(() => {
+    if (props.isSelected) {
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [props.isSelected]);
+
+  function onChange(param: { x: any; width: number; y: any; height: number }) {
+    setPosition({
+      x: param.x,
+      y: param.y,
+      width: param.width,
+      height: param.height
+    })
+  }
+
+  return (
+    <React.Fragment>
+      <Rect
+        width={ position.width }
+        height={ position.height }
+        onClick={ props.onSelect }
+        onTap={ props.onSelect }
+        ref={ shapeRef }
+        x={ position.x }
+        y={ position.y }
+        draggable
+        fill={ isDragging ? 'green' : 'black' }
+        onDragStart={ () => {
+          setIsDragging(true)
+        } }
+        onDragEnd={ (e) => {
+          setIsDragging(false);
+          setPosition({
+            ...position,
+            x: e.target.x(),
+            y: e.target.y(),
+          })
+        } }
+        onTransformEnd={ (e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            x: node.x(),
+            y: node.y(),
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        } }
+      />
+      { props.isSelected && (
+        <Transformer
+          ref={ trRef }
+          boundBoxFunc={ (oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+
+            return newBox;
+          } }
+        />
+      ) }
+    </React.Fragment>
+  );
+
+}
+
+export default FkRect;
