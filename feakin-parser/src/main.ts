dccf1bc273a908bacb4b178ddb445d737a640c96
Phodal Huang <h@phodal.com>
2022-07-14 19:53:09 +0800

    build: init parser for projects

---
+++ feakin-parser/src/main.ts
@@ -0,0 +1,32 @@
+/**
+ * Some predefined delay values (in milliseconds).
+ */
+export enum Delays {
+  Short = 500,
+  Medium = 2000,
+  Long = 5000,
+}
+
+/**
+ * Returns a Promise<string> that resolves after a given time.
+ *
+ * @param {string} name - A name.
+ * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
+ * @returns {Promise<string>}
+ */
+function delayedHello(
+  name: string,
+  delay: number = Delays.Medium,
+): Promise<string> {
+  return new Promise((resolve: (value?: string) => void) =>
+    setTimeout(() => resolve(`Hello, ${name}`), delay),
+  );
+}
+
+// Below are examples of using ESLint errors suppression
+// Here it is suppressing a missing return type definition for the greeter function.
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function greeter(name: string) {
+  return await delayedHello(name, Delays.Long);
+}
