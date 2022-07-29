29aa54f8729e64b22fdb356084efe5164e7fd600
Phodal Huang <h@phodal.com>
2022-07-29 23:23:54 +0800

    refactor: try to parse mermaid

--- packages/feakin-exporter/src/source/mermaid-flow.spec.ts
+++ packages/feakin-exporter/src/source/mermaid-flow.spec.ts
@@ -3,6 +3,5 @@ import { flow } from './mermaid-flow';
 describe('feakinExporter', () => {
   it('should work', () => {
     flow("graph TD; A-->B;");
-    // flow("graph TD; A-->B;").should.equal('feakin-exporter');
   });
 });
--- packages/feakin-exporter/src/source/mermaid-flow.ts
+++ packages/feakin-exporter/src/source/mermaid-flow.ts
@@ -1,3 +1,16 @@
-export function flow(str: string) {
+const Converter = require("@feakin/converter");
 
+export function flow(str: string) {
+  let flowParser = Converter.flowParser;
+  // todo: add mermaid for parser;
+  flowParser.parser.yy = {
+    lex: {
+      firstGraph: () => {
+        return true;
+      }
+    }
+  };
+
+  // return flowParser.parse(str);
+  return '';
 }
