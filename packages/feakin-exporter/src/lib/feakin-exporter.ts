9256bdcef50fc51f8cf786a566f70b4c2878abd1
Phodal Huang <h@phodal.com>
2022-08-02 20:57:15 +0800

    feat: add model for design

--- packages/feakin-exporter/src/lib/feakin-exporter.ts
+++ packages/feakin-exporter/src/lib/feakin-exporter.ts
@@ -1,3 +1,28 @@
+import * as dagre from 'dagre'
+import { Point } from "../model/geometry/point";
+
 export function feakinExporter(): string {
   return 'feakin-exporter';
+}
+
+export interface BaseEdge {
+  points: Point[]
+}
+
+export interface Valued {
+  label?: string;
+}
+
+export interface BaseNode extends Point, Valued {
+  width: number;
+  height: number;
+  padding?: number | undefined;
+  paddingX?: number | undefined;
+  paddingY?: number | undefined;
+  rx?: number | undefined;
+  ry?: number | undefined;
+}
+
+export function fromDagre(node: dagre.Node<string>): BaseNode {
+  return node;
 }
