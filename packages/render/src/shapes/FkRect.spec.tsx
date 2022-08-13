f07b49daf898162a2d5b1b01038d0587e489c21a
Phodal Huang <h@phodal.com>
2022-08-13 23:49:50 +0800

    test: add test for text width

--- packages/render/src/shapes/FkRect.spec.tsx
+++ packages/render/src/shapes/FkRect.spec.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import FkRect from "./FkRect";
 
 function getTextWidthWithFontSize(text: string, fontSize: number) {
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d")!;
-  context.font = `${fontSize}px Arial`;
+  context.font = `${ fontSize }px Arial`;
   return context.measureText(text).width;
 }
 
-test('renders learn react link', () => {
-  // render(<FkRect position={ {
-  //   x: 0,
-  //   y: 0,
-  //   width: 100,
-  //   height: 100,
-  // }
-  // }/>);
-
-
+test('render text size', () => {
   let fontSize = getTextWidthWithFontSize('Hello', 10);
   expect(fontSize).toBe(23);
+});
+
+test('render FkRect', () => {
+  render(<FkRect position={ {
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100,
+  } } node={{} as any}/>);
 });
