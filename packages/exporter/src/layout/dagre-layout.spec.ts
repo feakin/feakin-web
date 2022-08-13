8d479be8f19c946c596c5273a5e3ca2fdf7a5219
Phodal Huang <h@phodal.com>
2022-08-13 15:24:37 +0800

    test: add lost test for dagre layout

--- packages/exporter/src/layout/dagre-layout.spec.ts
+++ packages/exporter/src/layout/dagre-layout.spec.ts
@@ -16,4 +16,17 @@ describe('feakinExporter', () => {
     expect(layout.nodes[0].height).toBe(40);
     expect(layout.edges.length).toBe(1);
   });
+
+  it('target should have width and height', () => {
+    const relations: DagreRelation[] = [
+      { source: { name: 'A' } },
+      { source: { name: 'B' } },
+      { source: { name: 'B' }, target: { name: 'C' } },
+    ]
+
+    const layout = dagreLayout(relations);
+
+    expect(layout.nodes[2].width).toBe(100);
+    expect(layout.nodes[2].height).toBe(40);
+  });
 });
