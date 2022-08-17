44da6d73076c36fef127d4fab3c8388fada6917a
Phodal Huang <h@phodal.com>
2022-08-17 08:26:21 +0800

    test: fix lint

--- packages/exporter/src/model/shape/circle-shape.ts
+++ packages/exporter/src/model/shape/circle-shape.ts
@@ -2,7 +2,6 @@ import { Shape } from "./shape";
 import { ShapeType } from "./shape-type";
 
 export class CircleShape extends Shape {
-  radius: number;
   override type = ShapeType.Circle;
 
   constructor(x = 0, y = 0, radius = 0) {
