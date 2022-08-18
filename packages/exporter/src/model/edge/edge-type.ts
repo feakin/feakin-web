2e32b96776128a4d0c0401f7277ab7a6aa9e142b
Phodal Huang <h@phodal.com>
2022-08-18 21:56:14 +0800

    docs: fix type for edge

---
+++ packages/exporter/src/model/edge/edge-type.ts
@@ -0,0 +1,34 @@
+export enum EdgeType {
+  LINE = 'line',
+  ARROW = 'arrow',
+  POLYLINE = 'polyline',
+  CURVE = 'curve',
+  SCRIBBLE = 'scribble',
+}
+
+export enum EdgeConnectorType {
+  ELBOW = 'elbow',
+  CURVE = 'curve',
+}
+
+export enum LineStyle {
+  SOLID = 'solid',
+  DOT = 'dot',
+  DASH = 'dash',
+  DOT_DASH = 'dot-dash',
+  LONG_DASH = 'long-dash',
+  LONG_DASH_DOT = 'long-dash-dot',
+}
+
+export enum ArrowType {
+  NONE = 'none',
+  NOTCHED = 'notched',
+  HOLLOW = 'hollow',
+  HOLLOW_CIRCLE = 'hollow-circle',
+  HOLLOW_SQUARE = 'hollow-square',
+  HOLLOW_DIAMOND = 'hollow-diamond',
+  FILLED = 'filled',
+  FILLED_CIRCLE = 'filled-circle',
+  FILLED_SQUARE = 'filled-square',
+  FILLED_DIAMOND = 'filled-diamond',
+}
