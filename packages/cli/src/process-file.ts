d17808163d9c933001ead5cb6af94c3bc7c3efab
Phodal Huang <h@phodal.com>
2022-08-22 22:44:46 +0800

    chore: udpate version for test

--- packages/cli/src/process-file.ts
+++ packages/cli/src/process-file.ts
@@ -1,5 +1,6 @@
-import * as path from "path";
 import * as fs from "fs";
+import * as path from "path";
+
 import { DrawioConverter, DrawioEncode, ExcalidrawExporter, Executor, Graph } from "@feakin/exporter";
 
 function mermaidToExcalidraw(inputContent: string, outputFile: string) {
