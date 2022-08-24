eb214df8c988e23e97f0dc2473ed38ff85b049d3
Phodal Huang <h@phodal.com>
2022-08-24 21:20:39 +0800

    feat(ui): init basic menus

--- packages/render/src/App.tsx
+++ packages/render/src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import Render from "./Render";
+import { Button, Menu, MenuItem, TextareaAutosize } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 const App = () => {
+  const [fileEl, setFileEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(fileEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setFileEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setFileEl(null);
+  };
+
+  const importFile = () => {
+    console.log("importFile");
+  }
+
   const text = `graph TD;
     A-->B
     A-->C
@@ -9,7 +24,65 @@ const App = () => {
 
   return (
     <div>
-      <Render text={ text }/>
+      <Grid2 container spacing={3}>
+        <Grid2 xs={6} md={6}>
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            File
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={fileEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={importFile}>Import</MenuItem>
+          </Menu>
+        </Grid2>
+        <Grid2 xs={6}>
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            Export
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={fileEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={importFile}>Dot</MenuItem>
+            <MenuItem onClick={importFile}>Graphviz</MenuItem>
+            <MenuItem onClick={importFile}>Mermaid</MenuItem>
+            <MenuItem onClick={importFile}>Excalidraw</MenuItem>
+          </Menu>
+        </Grid2>
+        <Grid2 xs={6}>
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={6}
+            placeholder="Minimum 3 rows"
+          />
+        </Grid2>
+        <Grid2 xs={6}>
+          <Render text={ text }/>
+        </Grid2>
+      </Grid2>
     </div>
   );
 };
