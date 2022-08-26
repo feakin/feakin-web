e48b2170ba0b46c27343260f233091a027d6b8e2
Phodal Huang <h@phodal.com>
2022-08-26 09:06:08 +0800

    feat(db): add limit for history

--- packages/render/src/repository/change-history.ts
+++ packages/render/src/repository/change-history.ts
@@ -17,11 +17,32 @@ export class ChangeHistory {
 
   }
 
-  countHistory() {
+  limitHistorySize(size: number = 50) {
+    this.db.allDocs().then((response: PouchDB.Core.AllDocsResponse<any>) => {
+      if (response.total_rows <= size) {
+        return;
+      }
 
+      console.log(response);
+
+      this.db.allDocs({
+        limit: response.total_rows - size
+      }).then((response: PouchDB.Core.AllDocsResponse<any>) => {
+        response.rows.forEach((row: any) => {
+          this.db.remove(row.id, row.value.rev).then((response: any) => {
+            // handle response
+          }).catch((err: any) => {
+            console.log(err);
+          });
+        })
+      }).catch((err: any) => {
+        console.log(err);
+      });
+    });
   }
 
   save(code: string) {
+    this.limitHistorySize();
     this.db.put({
       _id: this.id(),
       title: 'local',
