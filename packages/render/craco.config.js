b1ab77ef70dd61c93a75781119bcc68f19bf0a86
Phodal Huang <h@phodal.com>
2022-08-29 22:09:09 +0800

    build: fix ignore warnings

--- packages/render/craco.config.js
+++ packages/render/craco.config.js
@@ -43,6 +43,17 @@ module.exports = {
         }
       });
 
+      config[ "ignoreWarnings" ] = [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes("node_modules") &&
+            warning.details &&
+            warning.details.includes("source-map-loader")
+          );
+        },
+      ]
+
       return config;
     },
   },
