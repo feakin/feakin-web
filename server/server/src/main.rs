db7746d7511bc9d91c78699e3f6f9ffe1c8d148d
Phodal Huang <h@phodal.com>
2022-09-03 20:06:09 +0800

    feat(server): init actix web app

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/api/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+  format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new().service(greet)
+  })
+    .bind(("127.0.0.1", 8804))?
+    .run()
+    .await
 }
