4ec7bb1d76caa6d2744dd498f4655b11a818da19
Phodal Huang <h@phodal.com>
2022-09-04 10:21:16 +0800

    docs: add delete for samples

--- server/server/src/live_coding.rs
+++ server/server/src/live_coding.rs
@@ -1,12 +1,13 @@
+use std::ops::Range;
+
 use diamond_types::list::OpLog;
-use diamond_types::list::encoding::ENCODE_FULL;
 use diamond_types::LocalVersion;
 
 pub struct LiveCoding {
   oplog: OpLog,
 }
 
-impl  LiveCoding {
+impl LiveCoding {
   fn new(content: &str, agent_name: Option<&str>) -> Result<Self, String> {
     let mut oplog = OpLog::new();
     let agent = oplog.get_or_create_agent_id(&agent_name.ok_or("root").unwrap());
@@ -24,7 +25,11 @@ impl  LiveCoding {
   fn insert(&mut self, agent_name: &str, pos: usize, content: &str) {
     let agent = self.oplog.get_or_create_agent_id(agent_name);
     self.oplog.add_insert(agent, pos, content);
-    self.oplog.encode(ENCODE_FULL);
+  }
+
+  fn delete(&mut self, agent_name: &str, range: Range<usize>) {
+    let agent = self.oplog.get_or_create_agent_id(agent_name);
+    self.oplog.add_delete_without_content(agent, range);
   }
 }
 
@@ -33,7 +38,7 @@ mod tests {
   use crate::live_coding::LiveCoding;
 
   #[test]
-  fn it_works() {
+  fn simulate_insert() {
     let agent1 = "phodal";
     let agent2 = "hello";
 
@@ -47,5 +52,23 @@ mod tests {
     assert_eq!(version.len(), 1);
     let branch = coding.oplog.checkout(&version);
     assert_eq!(branch.content().to_string(), "abzerzeroocdef");
+  }
+
+  #[test]
+  fn simulate_delete() {
+    let agent1 = "phodal";
+    let agent2 = "hello";
+
+    let mut live = LiveCoding::new("abcdef", Some("root")).unwrap();
+
+    live.insert(agent1, 2, "zero");
+
+    live.add_client(agent2);
+    live.delete(agent2, 2..8);
+
+    let local = live.oplog.local_version();
+    let branch = live.oplog.checkout(&local);
+
+    assert_eq!(branch.content().to_string(), "abef");
   }
 }
