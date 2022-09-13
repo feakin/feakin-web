8dad2a2c776f1baa94351a61983fcf3e158e4cda
Phodal Huang <h@phodal.com>
2022-09-13 20:38:09 +0800

    docs: update contet

--- server/fkl-parser/src/parser/ast.rs
+++ server/fkl-parser/src/parser/ast.rs
@@ -4,19 +4,19 @@ use serde::Serialize;
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ContextMap {
   pub name: String,
-  pub contexts: FkContext,
-  pub relations: Vec<BoundedContextRel>
+  pub contexts: BoundedContext,
+  pub relations: Vec<BoundedContextRelation>
 }
 
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct FkContext {
+pub struct BoundedContext {
   pub name: String,
 }
 
 // BoundedContextRelation
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub enum BoundedContextRel {
+pub enum BoundedContextRelation {
   // Symmetric relation
   SharedKernel,
   Partnership,
@@ -28,4 +28,36 @@ pub enum BoundedContextRel {
   AntiCorruptionLayer,
   OpenHostService,
   PublishedLanguage,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Aggregate {
+  pub name: String,
+  pub is_root: bool,
+  pub context: String,
+  pub fields: Vec<Field>,
+  pub commands: Vec<DomainCommand>,
+  pub events: Vec<DomainEvent>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct DomainCommand {
+  pub name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct DomainEvent {
+  pub name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct  Entity {
+  pub name: String,
+  pub fields: Vec<Field>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct  Field {
+  pub name: String,
+  pub type_: String,
 }
