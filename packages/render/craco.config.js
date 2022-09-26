9e5e8708b205619400e369bd4a8e0dc8c844fd9c
Phodal Huang <h@phodal.com>
2022-09-26 20:53:39 +0800

    build: clean codes

--- packages/render/craco.config.js
+++ packages/render/craco.config.js
@@ -3,34 +3,10 @@ const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
-// const webpack = require('webpack');
 
 module.exports = {
   webpack: {
     configure: (config) => {
-      // const wasmExtensionRegExp = /\.wasm$/;
-      // config.resolve.extensions.push('.wasm');
-      // config.experiments = {
-      //   asyncWebAssembly: false,
-      //   lazyCompilation: true,
-      //   syncWebAssembly: true,
-      //   topLevelAwait: true,
-      // };
-      // config.resolve.fallback = {
-      //   buffer: require.resolve('buffer/')
-      // }
-      // config.module.rules.forEach((rule) => {
-      //   (rule.oneOf || []).forEach((oneOf) => {
-      //     if (oneOf.type === "asset/resource") {
-      //       oneOf.exclude.push(wasmExtensionRegExp);
-      //     }
-      //   });
-      // });
-      // config.plugins.push(new webpack.ProvidePlugin({
-      //   Buffer: ['buffer', 'Buffer'],
-      // }));
-
-
       // Remove guard against importing modules outside of `src`.
       // Needed for workspace projects.
       config.resolve.plugins = config.resolve.plugins.filter(
@@ -55,7 +31,7 @@ module.exports = {
         // zlib: require.resolve("browserify-zlib"),
         stream: require.resolve("stream-browserify"),
         // util: require.resolve("util"),
-        // buffer: require.resolve("buffer"),
+        buffer: require.resolve("buffer"),
         // asset: require.resolve("assert"),
       }
 
